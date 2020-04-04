d26536e6a55980285a82d43b63ef3db8e6c1d043
easontiger <48283248+easontiger@users.noreply.github.com>
2020-04-04 19:54:42 +0800

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -158,11 +158,49 @@ document.getElementById("canvas").onmousemove=function(e){
 	start=new Point(e.pageX,e.pageY);
 }
 
-document.getElementById("canvas").ontouchstart=function(){
-	document.write(1);
+document.getElementById("canvas").onmousedown=function(e){
+	start=new Point(e.pageX,e.pageY);
 };
-document.getElementById("canvas").ontouchmove=document.getElementById("canvas").onmousemove;
-document.getElementById("canvas").ontouchend=document.getElementById("canvas").onmouseup;
+
+document.getElementById("canvas").ontouchmove=function(e){
+	if(start==null||pen!="0")return;
+	var sx=start.x,sy=start.y,ex=e.pageX,ey=e.pageY;
+	can.beginPath();
+	switch(reflect){
+		case "4":
+			can.moveTo(width-sx,height-sy);
+			can.lineTo(width-ex,height-ey);
+			can.moveTo(sx,height-sy);
+			can.lineTo(ex,height-ey);
+		case "2v":
+			can.moveTo(width-sx,sy);
+			can.lineTo(width-ex,ey);
+			can.moveTo(sx,sy);
+			can.lineTo(ex,ey);
+			break;
+		case "2p":
+			can.moveTo(sx,height-sy);
+			can.lineTo(ex,height-ey);
+		case "1":
+			can.moveTo(sx,sy);
+			can.lineTo(ex,ey);
+	}
+	can.stroke();
+	start=new Point(e.pageX,e.pageY);
+}
+
+document.getElementById("canvas").ontouchend=function(e){
+    if(start==null||pen!="1"){
+		start=null;
+		return;
+	}
+	var end=new Point(e.pageX,e.pageY);
+	var list_start=reflect_p(start),list_end=reflect_p(end);
+	for(i in list_end){
+		process(list_start[i],list_end[i]);
+	}
+	start=null;
+};
 
 document.getElementById("canvas").onmouseleave=function(){
 	start=null;
